5ca0dc209fc7a2fc32c0733d3f4686ef0a952a81
tomoyaonodera <hal_tokyo_onoderatomoya@yahoo.co.jp>
2016-10-25 12:23:47 +0900

    T.O

---
+++ TomoyaOnodera/Oceans Savior/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	private GUIStyle labelStyle;
+	Quaternion start_gyro;
+	Quaternion gyro;
+	void Start()
+	{
+		this.labelStyle = new GUIStyle();
+		this.labelStyle.fontSize = Screen.height / 22;
+		this.labelStyle.normal.textColor = Color.white;
+	}
+
+
+	void Update ()
+	{
+		Input.gyro.enabled = true;
+		if (Input.gyro.enabled)
+		{
+			gyro = Input.gyro.attitude;
+			gyro = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x,-gyro.y, gyro.z, gyro.w));
+			this.transform.localRotation = gyro;
+			//最初に見ていた向きとゲームの進行方向を合わせる
+			//this.transform.localRotation = Quaternion.Euler(0, -start_gyro.y, 0);
+		}
+	}
+}
+
---
+++ TomoyaOnodera/Oceans Savior/Assets/Scripts/GyroRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class GyroRotate : MonoBehaviour
+{
+    float yRotation; float xRotation;
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        yRotation += -Input.gyro.rotationRateUnbiased.y; xRotation += -Input.gyro.rotationRateUnbiased.x;
+
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
+    }
+}
