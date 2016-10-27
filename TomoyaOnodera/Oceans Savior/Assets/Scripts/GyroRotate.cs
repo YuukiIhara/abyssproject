8d741cfb647f893481c4915b9cb771541ac27735
tomoyaonodera <hal_tokyo_onoderatomoya@yahoo.co.jp>
2016-10-27 16:21:05 +0900

    ジャンプ台の実装。 GyroRotate.csのプレイヤーの移動量をPublic変数に変更、

--- TomoyaOnodera/Oceans Savior/Assets/Scripts/GyroRotate.cs
+++ TomoyaOnodera/Oceans Savior/Assets/Scripts/GyroRotate.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 public class GyroRotate : MonoBehaviour
 {
+	public float Speed;
     float yRotation; float xRotation;
 	Vector3 vec;
 
@@ -17,7 +18,7 @@ public class GyroRotate : MonoBehaviour
     {
 		//カメラの向いている方向に移動
 		vec = transform.localEulerAngles;
-		vec = new Vector3 (Mathf.Sin(vec.y*(Mathf.PI/180.0f))/20.0f, 0.0f,Mathf.Cos(vec.y*(Mathf.PI/180.0f))/20.0f);
+		vec = new Vector3 (Mathf.Sin(vec.y*(Mathf.PI/180.0f))/Speed, 0.0f,Mathf.Cos(vec.y*(Mathf.PI/180.0f))/Speed);
 		transform.position += vec;
 
 		// 加速度をカメラに反映する
