74de874bb17dfaf629002cc34926d3bbab555a05
tomoyaonodera <hal_tokyo_onoderatomoya@yahoo.co.jp>
2016-10-27 15:51:10 +0900

    カメラの向いている方向に進む処理を実装。

--- TomoyaOnodera/Oceans Savior/Assets/Scripts/GyroRotate.cs
+++ TomoyaOnodera/Oceans Savior/Assets/Scripts/GyroRotate.cs
@@ -3,15 +3,28 @@ using System.Collections;
 public class GyroRotate : MonoBehaviour
 {
     float yRotation; float xRotation;
+	Vector3 vec;
+
     void Start()
     {
+		// ジェイロ機能を使用する
         Input.gyro.enabled = true;
+		// 初期化
+		vec = new Vector3(0.0f,0.0f,0.0f);
     }
 
     void Update()
     {
-        yRotation += -Input.gyro.rotationRateUnbiased.y; xRotation += -Input.gyro.rotationRateUnbiased.x;
+		//カメラの向いている方向に移動
+		vec = transform.localEulerAngles;
+		vec = new Vector3 (Mathf.Sin(vec.y*(Mathf.PI/180.0f))/20.0f, 0.0f,Mathf.Cos(vec.y*(Mathf.PI/180.0f))/20.0f);
+		transform.position += vec;
 
-        transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
+		// 加速度をカメラに反映する
+		yRotation += -Input.gyro.rotationRateUnbiased.y; 
+		xRotation += -Input.gyro.rotationRateUnbiased.x;
+		transform.eulerAngles = new Vector3(xRotation,yRotation, 0);
+
     }
+
 }
