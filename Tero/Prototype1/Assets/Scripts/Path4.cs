f4e3de60808074be2df40c293688c1cdeb33b7df
Tero <tjm00se@yahoo.se>
2016-11-11 13:10:46 +0900

    Alpha

---
+++ Tero/Prototype1/Assets/Scripts/Path4.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Path4 : MonoBehaviour {
+	private bool once = false;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.z>=311)
+		{
+			if (!once)
+			{
+				iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Path4"), "time", 25, "easetype", iTween.EaseType.linear));
+				once = true;
+			}
+		}
+	}
+}
