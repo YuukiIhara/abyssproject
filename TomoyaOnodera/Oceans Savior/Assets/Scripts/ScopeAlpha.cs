a5a40ba78b7ce585fb7113779ab50119648cf957
derarin <derarinrin@yahoo.co.jp>
2016-12-01 14:11:04 +0900

    スコープのAlpha値設定

---
+++ TomoyaOnodera/Oceans Savior/Assets/Scripts/ScopeAlpha.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // uGUIの機能を使うお約束
+
+public class ScopeAlpha : MonoBehaviour {
+	
+	// α値の設定
+	[SerializeField][Range(0.000f, 1.0f)]
+	float fAlpha = 0.5f;
+	// レンダラーの宣言
+	new CanvasRenderer renderer;
+
+	void Start ()
+	{
+		// レンダラーの取得
+		renderer = GetComponent<CanvasRenderer> ();
+		// 透明処理
+		renderer.SetAlpha(fAlpha);
+	}
+
+	void Update () 
+	{
+	
+	}
+}
