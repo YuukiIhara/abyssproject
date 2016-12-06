d4cdf3d14a8bf3044bbd7519c4935e9cfbc28ed9
derarin <derarinrin@yahoo.co.jp>
2016-12-06 12:42:03 +0900

    スコアの追加

---
+++ TomoyaOnodera/OceansSavior/Assets/Scripts/EnergyGenerator.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+/*
+/// <summary>
+/// 独自のUI描画
+/// </summary>
+[RequireComponent( typeof( CanvasRenderer ) )]
+[RequireComponent( typeof( RectTransform ) )]
+public class EnergyGenerator
+	: Graphic
+{
+	// 変数宣言
+
+
+	/// <summary>
+	/// メッシュに設定したいテクスチャをここで指定
+	/// </summary>
+	[SerializeField]
+	private Texture texture_;
+
+	/// <summary>
+	/// メッシュに設定するテクスチャの指定
+	/// </summary>
+	public override Texture mainTexture
+	{
+		get
+		{
+			// ここで設定したいテクスチャを返すようにする
+			return texture_;
+		}
+	}
+
+	/// <summary>
+	/// ここでメッシュを生成する
+	/// </summary>
+	/// <param name="vbo">Vbo.</param>
+	protected override void OnPopulateMesh( Mesh mesh )
+	{
+		
+		// 1左
+		UIVertex l1 = UIVertex.simpleVert;
+		l1.position = new Vector3( 0, 20, 0 );
+		l1.uv0 = new Vector2(1,1);
+		//lt.color = Color.green;
+
+		// 1右
+		UIVertex r1 = UIVertex.simpleVert;
+		r1.position = new Vector3( 100, 20, 0 );
+		r1.uv0 = new Vector2(0,1);
+		//rt.color = Color.red;
+
+		// 2左
+		UIVertex l2 = UIVertex.simpleVert;
+		l2.position = new Vector3( 0, 0, 0 );
+		l2.uv0 = new Vector2(1,0);
+		//lb.color = Color.white;
+
+		// 2右
+		UIVertex r2 = UIVertex.simpleVert;
+		r2.position = new Vector3( 100, 0, 0 );
+		r2.uv0 = new Vector2(0,0);
+		//rb.color = Color.yellow;
+
+		using ( var vb0 = new VertexHelper() ) {
+			vb0.AddUIVertexQuad( new UIVertex[] { l2, r2, r1, l1 } );
+			vb0.FillMesh( mesh );
+		}
+	}
+}*/
---
+++ TomoyaOnodera/OceansSavior/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // uGUIの機能を使うお約束
+
+public class ScoreManager : MonoBehaviour {
+
+	const int	SCORE_DIGIT_MAX=4; 
+	const float LERPTIME_MAX=1.0f;
+	const float ADD_LERPTIME=0.1f;
+	private int Score = 0;
+	public int DestScore;
+	private int FromScore=0;
+	private float LerpTime=0.0000f;
+	//private GameObject[] ScoreObject=new GameObject[SCORE_DIGIT_MAX];
+	//private SpriteRenderer[] ScoreRenderer=new SpriteRenderer[SCORE_DIGIT_MAX];
+	public GameObject[] ScoreRenderer;
+	public Sprite[] ScoreSprite=new Sprite[10];
+	// Use this for initialization
+	// イメージの宣言
+	Image image;
+
+	void Start ()
+	{
+		// スコアの初期化
+		for (int i = 0; i < SCORE_DIGIT_MAX; i++) 
+		{
+			image = ScoreRenderer [i].GetComponent<Image> ();
+			image.sprite = ScoreSprite [0];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int score=0;
+		int num=1;
+		if(DestScore!=Score)
+		{
+			float fScore=Score;
+			float fFromScore=FromScore;
+			float fDestScore=DestScore;
+			fScore = Mathf.Lerp((float)fFromScore,(float)fDestScore,LerpTime);
+			LerpTime+=ADD_LERPTIME;
+			Score=(int)fScore;
+
+		}
+		//float score_tex_pos;
+		score=Score;
+		for(int i=0;i<SCORE_DIGIT_MAX-1;i++)
+		{
+			num=num*10;
+		}
+
+		for(int i=0;i<SCORE_DIGIT_MAX;i++)
+		{
+			// スプライトの配列設定
+			image = ScoreRenderer [i].GetComponent<Image>();
+
+			if(Score >= num)
+			{
+				image.sprite = ScoreSprite[score/num];
+			}
+			else
+			{
+				if(i==SCORE_DIGIT_MAX-1)
+				{
+					image.sprite = ScoreSprite[0];
+				}
+				else
+				{
+					image.sprite =ScoreSprite[10];
+				}
+			}
+			score=score%num;
+			num=num/10;
+		}
+
+	}
+	public void AddScore(int score)
+	{
+		DestScore +=score;
+		FromScore=Score;
+		LerpTime=0.0f;
+
+	}
+
+	public int GetScore()
+	{
+		return Score;
+	}
+}
+
+
